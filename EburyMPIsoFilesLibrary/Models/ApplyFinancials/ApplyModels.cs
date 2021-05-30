c9b9bdcb654582eabe77105f0717a857e2ba44a3
EdJohnstonG9 <Ed.Johnston@EngSyst.co.uk>
2021-05-30 19:10:54 +0100

    Added ApplyFinancials Service

---
+++ EburyMPIsoFilesLibrary/Models/ApplyFinancials/ApplyModels.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EburyMPIsoFilesLibrary.Models.ApplyFinancials
+{
+
+    public class AuthenticateResponse
+    {
+        public string status { get; set; }
+        public string comment { get; set; }
+        public string token { get; set; }
+    }
+
+    public class ConvertResponse
+    {
+        public string countryCode { get; set; }
+        public string nationalId { get; set; }
+        public string accountNumber { get; set; }
+        public string recommendedNatId { get; set; }
+        public string recommendedAcct { get; set; }
+        public string recommendedBIC { get; set; }
+        public string status { get; set; }
+        public string comment { get; set; }
+        public string _ref { get; set; }
+        public string group { get; set; }
+        public Branchdetail[] branchDetails { get; set; }
+        public Headofficedetails headOfficeDetails { get; set; }
+        public Paymentbicdetails paymentBicDetails { get; set; }
+        public string bic8 { get; set; }
+        public string dataStore { get; set; }
+        public string noBranch { get; set; }
+        public string isoAddr { get; set; }
+        public string payBranchType { get; set; }
+        public string freeToken { get; set; }
+    }
+
+    public class Headofficedetails
+    {
+        public string bankName { get; set; }
+        public string branch { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string postZip { get; set; }
+        public string region { get; set; }
+        public string country { get; set; }
+        public Codedetails codeDetails { get; set; }
+        public Additionaldata additionalData { get; set; }
+    }
+
+    public class Codedetails
+    {
+        public string codeName1 { get; set; }
+        public string codeValue1 { get; set; }
+        public string codeName2 { get; set; }
+        public string codeValue2 { get; set; }
+        public string codeName3 { get; set; }
+        public string codeValue3 { get; set; }
+        public string codeName4 { get; set; }
+        public string codeValue4 { get; set; }
+    }
+
+    public class Additionaldata
+    {
+        public string ssiAvailable { get; set; }
+        public string payServiceAvailable { get; set; }
+        public string contactsAvailable { get; set; }
+        public string messageAvailable { get; set; }
+        public string holidayAvailable { get; set; }
+    }
+
+    public class Paymentbicdetails
+    {
+        public string branchTypeLabel { get; set; }
+        public string bankName { get; set; }
+        public string branch { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string postZip { get; set; }
+        public string region { get; set; }
+        public string country { get; set; }
+        public Codedetails codeDetails { get; set; }
+        public Additionaldata additionalData { get; set; }
+    }
+
+    public class Branchdetail
+    {
+        public string bankName { get; set; }
+        public string branch { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string postZip { get; set; }
+        public string region { get; set; }
+        public string country { get; set; }
+        public Codedetails codeDetails { get; set; }
+        public Sepadetails sepaDetails { get; set; }
+        public Additionaldata additionalData { get; set; }
+        public string bankToken { get; set; }
+    }
+
+    public class Sepadetails
+    {
+        public string ctStatus { get; set; }
+        public string ddStatus { get; set; }
+        public string bbStatus { get; set; }
+    }
+
+}
---
+++ EburyMPIsoFilesLibrary/Services/ApplyFinancialsService.cs
@@ -0,0 +1,102 @@
+﻿using EburyMPIsoFilesLibrary.Models.ApplyFinancials;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security;
+using System.Text;
+
+namespace EburyMPIsoFilesLibrary.Services
+{
+    public class ApplyFinancialsService
+    {
+        const string basePath = @"https://apps.applyfinancial.co.uk/validate-api/rest";
+
+        public string Token { get; private set; }
+
+        #region authenticate
+        public HttpStatusCode Authenticate(string username, SecureString password)
+        {
+            RestClient client = new RestClient(basePath);
+            var request = getAuthenticateRequest(username, password);
+            var response = client.Post(request);
+            Token = authToken(response);
+            return response.StatusCode;
+        }
+        private RestRequest getAuthenticateRequest(string username, SecureString password)
+        {
+            string reqPath = @"/authenticate";
+            Method method = Method.POST;
+
+            var credential = new NetworkCredential(username, password);
+            var request = new RestRequest(reqPath, method);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("username", credential.UserName, "application/x-www-form-urlencoded", ParameterType.GetOrPost);
+            request.AddParameter("password", credential.Password, "application/x-www-form-urlencoded", ParameterType.GetOrPost);
+
+            return request;
+        }
+        private string authToken(IRestResponse response)
+        {
+            string output = null;
+            if(response.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    AuthenticateResponse auth = JsonConvert.DeserializeObject<AuthenticateResponse>(response.Content);
+                    output = auth.token;
+                }
+                catch (Exception)
+                {
+                }
+            }
+            return output;
+        }
+        #endregion
+
+        #region convert
+        public ConvertResponse Convert(string countryCode, string branchId, string accountNo)
+        {
+            RestClient client = new RestClient(basePath);
+            var request = getConvertRequest(countryCode,branchId,accountNo);
+            var response = client.Get(request);
+            var output = getConvert(response);
+            return output;
+        }
+
+        private RestRequest getConvertRequest(string countryCode, string nationalId, string accountNumber)
+        {
+            
+            string reqPath = @"/convert/1.0.1";
+            Method method = Method.GET;
+
+            var request = new RestRequest(reqPath, method);
+            request.AddParameter("countryCode", countryCode, ParameterType.QueryString);
+            request.AddParameter("nationalId", nationalId, ParameterType.QueryString);
+            request.AddParameter("accountNumber", accountNumber, ParameterType.QueryString);
+            request.AddParameter("token", Token, ParameterType.QueryString);
+
+            return request;
+        }
+        private ConvertResponse getConvert(IRestResponse response)
+        {
+            ConvertResponse output = null;
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    ConvertResponse convert = JsonConvert.DeserializeObject<ConvertResponse>(response.Content);
+                    output = convert;
+                }
+                catch (Exception)
+                {
+                }
+            }
+            return output;
+        }
+
+
+        #endregion
+    }
+}
