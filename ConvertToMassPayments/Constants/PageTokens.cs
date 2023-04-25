4be1c86ee75e6a70dfdb84dfeee861a94dbd669a
Ed Johnston <ed.johnston@engsyst.co.uk>
2023-04-25 13:29:26 +0100

    Clean up UWP

--- ConvertToMassPayments/Constants/PageTokens.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ConvertToMassPayments
-{
-    internal static class PageTokens
-    {
-        public const string ConvertPage = "Convert";
-        public const string SettingsPage = "Settings";
-    }
-}
